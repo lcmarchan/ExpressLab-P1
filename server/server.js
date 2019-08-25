93431f7a43516fd77b04873ba152c0d73ea7d624
iflyimpilot <iflyimpilot@gmail.com>
2019-08-25 15:27:46 -0400

    first commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require ("express");
+const app = express();
+const port = 5000;
+
+// const cartItemsRoutes = require
+// const cors = require("cors");
+
+// app.use(cors());
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
